0ef4017dee157c6b9b6ef38cdd7cede4c2a72199
Sushma Gopireddy <gopireddysushma@gmail.com>
2025-11-18 11:19:26 -0500

    connection

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 
 import express from 'express';
+import cors from 'cors';
 
 const app = express()
 
 const port = 8080;
+app.use(cors())
+
  app.get('/',(req, res) =>{
     res.json('Hello world! (from server)')
  })
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react'
+import './App.css' 
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+useEffect(() =>{
+  async function test() {
+    const response = await fetch('http://localhost:8080')
+    const result = await response.json()
+    console.log(result)
+  }
+  test()
+},[])
   return (
     <>
       <div>
-        
+
       </div>
 
     </>
