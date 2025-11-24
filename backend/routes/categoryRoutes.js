e9b85a79b6538077eb015e27a3f66e0dd0748520
Sushma Gopireddy <gopireddysushma@gmail.com>
2025-11-24 10:36:22 -0500

    added user authentication to expense and category routes

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 const router = Router();
 import { getCategories, createCategory, updateCategory, deleteCategory } from '../controllers/categoryController.js';
+import auth from '../middleware/auth.js';
+
+router.use(auth); 
 
 router.get('/', getCategories);
 router.post('/', createCategory);
--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 const router = Router();
 import { getExpenses, createExpense, updateExpense, deleteExpense, getExpenseStats } from '../controllers/expenseController.js';
+import auth from '../middleware/auth.js';
 
+router.use(auth); 
 
 router.get('/', getExpenses);
 router.post('/', createExpense);
